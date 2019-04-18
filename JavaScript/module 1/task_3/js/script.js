b20bb306c465e6e9d7ef933358e7b929eb8fdb7e
VladDlogush <vlad.dlogush21@gmail.com>
2019-04-18 14:21:57 +0300

    fix module 1 task_3

--- JavaScript/module 1/task_3/js/script.js
+++ JavaScript/module 1/task_3/js/script.js
@@ -1,29 +1,35 @@
 'use strict'
 
-const country = prompt('Введите свою страну: ').toLowerCase();
-let message;
+let country = prompt('Введите свою страну: ');
+
 const priceChina = 100;
 const priceSA = 250;
 const priceAustralia = 170;
 const priceIndia = 80;
 const priceYamaica = 120;
 
-switch (country) {
-  case 'китай':
-    message = confirm(`Доставка в ${country} будет стоить ${priceChina} кредитов`);
-    break;
-  case 'южная америка':
-    message = confirm(`Доставка в ${country} будет стоить ${priceSA} кредитов`);
-    break;
-  case 'австралия':
-    message = confirm(`Доставка в ${country} будет стоить ${priceAustralia} кредитов`);
-    break;
-  case 'индия':
-    message = confirm(`Доставка в ${country} будет стоить ${priceIndia} кредитов`);
-    break;
-  case 'ямайка':
-    message = confirm(`Доставка в ${country} будет стоить ${priceYamaica} кредитов`);
-    break;
-  default:
-    alert('В вашей стране доставка не доступна');
+if(country === null){
+  console.log('Отмена');
+}else{
+  country = country.toLowerCase();
+  switch (country) {
+    case 'китай':
+      console.log(`Доставка в ${country} будет стоить ${priceChina} кредитов`);
+      break;
+    case 'южная америка':
+      console.log(`Доставка в ${country} будет стоить ${priceSA} кредитов`);
+      break;
+    case 'австралия':
+      console.log(`Доставка в ${country} будет стоить ${priceAustralia} кредитов`);
+      break;
+    case 'индия':
+      console.log(`Доставка в ${country} будет стоить ${priceIndia} кредитов`);
+      break;
+    case 'ямайка':
+      console.log(`Доставка в ${country} будет стоить ${priceYamaica} кредитов`);
+      break;
+    default:
+      console.log('В вашей стране доставка не доступна');
+
+  }
 }
