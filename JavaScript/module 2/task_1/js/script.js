dfa9b05a129930c3811b0da9d62649923b8b0be9
VladDlogush <vlad.dlogush21@gmail.com>
2019-04-25 19:01:36 +0300

    fix module-2 task-1

--- JavaScript/module 2/task_1/js/script.js
+++ JavaScript/module 2/task_1/js/script.js
@@ -6,16 +6,13 @@ let total = 0;
 
 do {
   input = prompt('Введите число: ');
-  if(input !== null && input !== "" && isNaN(input) === false){
+  if(input !== null && input !== "" && !Number.isNaN(Number(input)) ){
     numbers.push(input);
-  }else if(isNaN(Number(input))){
+  }else if(Number.isNaN(Number(input))){
       alert('Было введено не число, попробуйте еще раз');
   }
-  if (input === null) {
-      console.log('Cancel');
-  }
 } while (input !== null);
-if (numbers.length !== 0){
+if (numbers.length){
   for (let element of numbers) {
      total += Number(element);
   }
